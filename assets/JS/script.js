d27557a7e964c4530976ad3be8e63f59297a34e2
Aaron <aaturner1995@gmail.com>
2023-02-19 17:08:29 -0400

    finished 5 day forecast and started work to save the searches

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -33,24 +33,37 @@ const searchForecast = (city) => {
 };
 
 const displayWeather = (data) => {
-    let resultCityEl = $('<h2>');
-    resultCityEl.text(data.name + ' (' + dayjs(data.dt * 1000).format('MM/DD/YYYY') + ')').append(`<img src="https://openweathermap.org/img/wn/` + data.weather[0].icon + `@2x.png"></img>`);
-    resultTempEl = $('<p>');
-    resultTempEl.text('Temperature: ' + data.main.temp + '°C');
-    resultWindEl = $('<p>');
-    resultWindEl.text('Wind: ' + data.wind.speed + ' km/h');
-    resultHumidEl = $('<p>');
-    resultHumidEl.text('Humidity: ' + data.main.humidity + '%');
-    currentResultEl.append(resultCityEl, resultTempEl, resultWindEl, resultHumidEl);
-}
+    let resultCityEl = $('<h2>').text(data.name + ' (' + dayjs(data.dt * 1000).format('MM/DD/YYYY') + ')').append(`<img src="https://openweathermap.org/img/wn/` + data.weather[0].icon + `@2x.png"></img>`);
+    resultTempEl = $('<p>').text('Temperature: ' + data.main.temp + '°C');
+    resultWindEl = $('<p>').text('Wind: ' + data.wind.speed + ' km/h');
+    resultHumidEl = $('<p>').text('Humidity: ' + data.main.humidity + '%');
+    currentResultEl.addClass('card px-2 mb-3').append(resultCityEl, resultTempEl, resultWindEl, resultHumidEl);
+};
 
 const displayForecast = (data) => {
+    fiveDayEl.empty();
+    const headerText = $('<h2>').text("Five Day Forecast");
+    const rowEl = $('<div>').addClass('row');
     data.list.forEach((e) => {
         if (e.dt_txt[12] === '2') {
-            
+            const iconResultEl = $('<img>').attr('src', `https://openweathermap.org/img/wn/` + e.weather[0].icon + `@2x.png`);
+            const dateResultEl = $('<h3>').text(dayjs(e.dt*1000).format('MM/DD/YYYY'));
+            const tempResultEl = $('<p>').text('Temperature: ' + e.main.temp + '°C');
+            const windResultEl = $('<p>').text('Wind: ' + e.wind.speed + ' km/h');
+            const humidResultEl = $('<p>').text('Humidity: ' + e.main.humidity + '%');
+            dateResultEl.append(iconResultEl);
+            const cardEl = $('<div>').addClass('col mt-1').append($('<div>').addClass('card p-2 bg-dark text-white').append(dateResultEl, tempResultEl, windResultEl, humidResultEl));
+            rowEl.append(cardEl);
         }
     })
+    fiveDayEl.append(headerText,rowEl);
+};
+
+
+const saveSearchCity = (city) => {
+    
 }
+
 
 const searchFormSub = (e) => {
     e.preventDefault();
